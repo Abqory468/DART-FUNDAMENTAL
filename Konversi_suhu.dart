fa1d7c8172e733ca6e9ea69e6b0b85b326850193
Abqory468 <qoryrezaidn@gmail.com>
2026-07-16 11:21:11 +0700

    feat: add temperature conversion program from Fahrenheit to Celsius

---
+++ Konversi_suhu.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+void main(List<String> args) {
+  stdout.write('Suhu Fahrenheit: ');
+  double suhuF = double.parse(stdin.readLineSync()!);
+
+  print('Hasil konversi ke Celcius: ${konversi(suhuF)}');
+}
+
+double konversi(double suhuF) {
+  double suhuC = (suhuF - 32) * 5 / 9;
+  return suhuC;
+}
