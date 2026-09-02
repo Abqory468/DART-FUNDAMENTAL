0e8bf43938c430f71ddf8507c7fd8bd013ca7898
Abqory468 <qoryrezaidn@gmail.com>
2026-09-02 10:56:46 +0700

    feat: add Plants class with attributes and constructor for object initialization

--- oop2/main.dart
+++ oop2/main.dart
@@ -1,9 +1,13 @@
 import 'animal.dart';
+import 'plants.dart';
 
 void main(List<String> args) {
   // SYNTAX: var namaObjek = namaClass(Parameter/Properties);
   var cat = Animal('Cana', 4.2, 'Gray');
+  var rose = Plants('Rose', 'Roselia Keminhu', 'Red');
 
   cat.eat();
   print(cat.weight);
+
+  
 }
--- oop2/plants.dart
+++ oop2/plants.dart
@@ -1,3 +1,17 @@
 class Plants {
-  
+  // Attribute/Properties
+  String? name;
+  String? latinName;
+  String? color;
+
+  // Constructor
+  // Membuat inisialisasi lebih mudah daripada satu-satu
+  // disebut juga method khusus karena berbeda dengan method biasa
+
+  // 1. Nama harus sama dengan Class
+  // 2. Tidak memiliki Kembalian (Return type)
+  // 3. dipanggil 'otomatis' ketika objek dibuat
+  // 4. digunakan untuk inisialisasi objek
+
+  Plants(this.name, this.latinName, this.color);
 }
